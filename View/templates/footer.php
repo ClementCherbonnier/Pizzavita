de86751401abf11241f6dc634b7cb153e68f67e6
doyla <94548932+doyla@users.noreply.github.com>
2021-12-08 15:20:20 +0100

    Update footer.php

--- View/templates/footer.php
+++ View/templates/footer.php
@@ -1,11 +1,30 @@
-<!doctype html>
-<html>
-<head>
-    <link rel="stylesheet" href="/Pizzavita/Ressources/Css/footer.css">
-</head>
-<body>
+<link rel="stylesheet" href="/Pizzavita/Ressources/Css/footer.css">
     <footer>
         <div class="copyright">
+            <p>Adresse: 69 Rue de l'Humour  77555 Maran </p>
+            <p>Numéro de Téléphone : 02 55 57 77 69</p>
+            
+            <div>
+                <!-- Facebook -->
+                <a href="https://www.facebook.com/KFC-343437626414239/" target="_blank">
+                    <img src="\Pizzavita\Ressources\facebook.png" width="40"/>
+                </a>
+                
+                <!-- Twitter -->
+                <a href="https://twitter.com/kfcfrance" target="_blank" role="button">
+                    <img src="\Pizzavita\Ressources\twitter.png" width="40"/>
+                </a>
+                
+                <!-- Instagram -->
+                <a href="https://www.instagram.com/kfc_france/?hl=fr" target="_blank">
+                    <img src="\Pizzavita\Ressources\instagram.png" width="40"/>
+                </a>
+                
+            </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5631.404363330271!2d102.78212218639683!3d3.586
+                    5068557968542!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31ced5e9c9333e5b%3A0x219a0f8470603cab!2
+                    sKFC%20Maran!5e0!3m2!1sen!2sfr!4v1637240761005!5m2!1sen!2sfr" width="500" height="200" style="border:0;" 
+                    allowfullscreen="" loading="lazy"></iframe>
             <p>Copyright © 2021 Pizzavita. All Right Reserved</p>
         </div>
     </footer>
