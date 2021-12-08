257541c4732e8a37d0f81bf05986015eed5e5650
ClementCherbonnier <Clemt72@gmail.com>
2021-12-08 13:00:27 +0100

    section Nos Valeurs

--- View/index.php
+++ View/index.php
@@ -18,5 +18,22 @@
     </div>
     <!-- <a href="/" class="btn">Entrer dans la ville</a> -->
   </section>
+  <section class="about" id="about">
+    <div class="title white">
+      <h2>Nos Valeurs</h2>
+      <p>La pizza est un art, on en fait notre passion</p>
+    </div>
+    <div class="content">
+      <div class="textBox">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempus augue nec scelerisque bibendum. Sed ut molestie mi. Phasellus blandit ultrices mauris eget dictum. Maecenas turpis leo, laoreet sed metus id, sodales gravida libero. Donec molestie massa a massa aliquam porta. Fusce nec leo et urna imperdiet convallis. Aliquam at luctus odio. Duis interdum sagittis luctus.
+        <br> <br>
+        Maecenas tincidunt leo eu ante scelerisque accumsan. Etiam luctus ut eros ut pulvinar. Ut at massa eu dolor efficitur fermentum. Suspendisse ornare magna facilisis lectus sodales, ac placerat leo dapibus. Vestibulum commodo, eros non iaculis volutpat, est sapien sodales nunc, non venenatis ante nibh a risus. Suspendisse sit amet lacus diam. Phasellus iaculis non ligula eget sodales. Phasellus ornare facilisis elit, facilisis sodales ipsum accumsan ut. Suspendisse eget odio a lacus tincidunt ultrices ut at metus. Aliquam efficitur odio euismod faucibus sodales. Vestibulum cursus sapien dignissim leo sagittis mollis. 
+        </p>
+      </div>
+      <div class="imgBox">
+        <img id="metwo" src="/Pizzavita/Ressources/teamPV.jpg">
+      </div>
+    </div>
+  </section>
 </body>
 </html>
