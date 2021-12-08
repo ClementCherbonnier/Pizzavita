5a487e55759cb37bb23204b2f88081f5c2df22bc
damienberton01 <damienberton01@gmail.com>
2021-12-08 14:25:19 +0100

    transformation en .php

---
+++ View/phototèque.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Phototèque</title>
+    </head>
+    <body>  
+        <div class="sectionBar">
+            <h4>Galerie bar</h4>
+            <img alt="../Ressources/bar1.jfif" src="../Ressources/bar1.jfif">
+            <img alt="../Ressources/bar2.jfif" src="../Ressources/bar2.jfif">
+        </div>
+        <div class="sectionSoiree">
+            <h4>Galerie soirée</h4>
+            <img alt="../Ressources/soiréePizza1.jfif" src="../Ressources/soiréePizza1.jfif">
+            <img alt="../Ressources/soiréePizza2.jfif" src="../Ressources/soiréePizza2.jfif">
+            <img alt="../Ressources/soiréePizza3.jfif" src="../Ressources/soiréePizza3.jfif">
+        </div >
+        <div class="sectionEvenement">
+            <h4>Galerie evenement</h4>
+            <img alt="../Ressources/evenement1.jfif" src="../Ressources/evenement1.jfif">
+            <img alt="../Ressources/evenement2.jfif" src="../Ressources/evenement2.jfif">
+        </div>
+    </body>
+</html>
