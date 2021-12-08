3f78ae869cbbb3d8cbf6e49c9e23af41714d01bb
doyla <94548932+doyla@users.noreply.github.com>
2021-12-08 15:21:07 +0100

    Update footer.php

--- View/templates/footer.php
+++ View/templates/footer.php
@@ -2,7 +2,7 @@
     <footer>
         <div class="copyright">
             <p>Adresse: 69 Rue de l'Humour  77555 Maran </p>
-            <p>Numéro de Téléphone : 02 55 57 77 69</p>
+            <p>Numéro de Téléphone : 02 55 57 77 69</p><br>
             
             <div>
                 <!-- Facebook -->
@@ -20,7 +20,7 @@
                     <img src="\Pizzavita\Ressources\instagram.png" width="40"/>
                 </a>
                 
-            </div>
+            </div><br>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5631.404363330271!2d102.78212218639683!3d3.586
                     5068557968542!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31ced5e9c9333e5b%3A0x219a0f8470603cab!2
                     sKFC%20Maran!5e0!3m2!1sen!2sfr!4v1637240761005!5m2!1sen!2sfr" width="500" height="200" style="border:0;" 
