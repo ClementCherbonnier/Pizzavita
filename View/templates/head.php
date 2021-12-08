d45159056353bf0dfa97a8dc7c173ae9841eb4d2
ClementCherbonnier <Clemt72@gmail.com>
2021-12-08 12:28:33 +0100

    ajout de bootstrap

--- View/templates/head.php
+++ View/templates/head.php
@@ -4,7 +4,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta charset="utf-8" />
   <link rel="icon" href="/Pizzavita/Ressources/favicon.ico" />
+  <link href="/Pizzavita/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <title>Pizzavita</title>
 </head>
+<body>
+  <!-- jQuery link -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Bootstrap JS -->
+  <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+</body>
 
 </html>
