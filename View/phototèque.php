b0339b3def04a7248b1282a65f0165aea23afbc7
damienberton01 <damienberton01@gmail.com>
2021-12-08 15:27:51 +0100

    maj css phototèque.php

--- View/phototèque.php
+++ View/phototèque.php
@@ -8,19 +8,19 @@
     <body>  
         <div class="sectionBar">
             <h4>Galerie bar</h4>
-            <img alt="../Ressources/bar1.jfif" src="/Pizzavita/Ressources/bar1.jfif">
-            <img alt="../Ressources/bar2.jfif" src="/Pizzavita/Ressources/bar2.jfif">
+            <img class="imagesPhototèque" alt="../Ressources/bar1.jfif" src="/Pizzavita/Ressources/bar1.jfif">
+            <img class="imagesPhototèque" alt="../Ressources/bar2.jfif" src="/Pizzavita/Ressources/bar2.jfif">
         </div>
         <div class="sectionSoiree">
             <h4>Galerie soirée</h4>
-            <img alt="../Ressources/soiréePizza1.jfif" src="/Pizzavita/Ressources/soiréePizza1.jfif">
-            <img alt="../Ressources/soiréePizza2.jfif" src="/Pizzavita/Ressources/soiréePizza2.jfif">
-            <img alt="../Ressources/soiréePizza3.jfif" src="/Pizzavita/Ressources/soiréePizza3.jfif">
+            <img class="imagesPhototèque" alt="../Ressources/soiréePizza1.jfif" src="/Pizzavita/Ressources/soiréePizza1.jfif">
+            <img class="imagesPhototèque" alt="../Ressources/soiréePizza2.jfif" src="/Pizzavita/Ressources/soiréePizza2.jfif">
+            <img class="imagesPhototèque" alt="../Ressources/soiréePizza3.jfif" src="/Pizzavita/Ressources/soiréePizza3.jfif">
         </div >
         <div class="sectionEvenement">
             <h4>Galerie evenement</h4>
-            <img alt="../Ressources/evenement1.jfif" src="/Pizzavita/Ressources/evenement1.jfif">
-            <img alt="../Ressources/evenement2.jfif" src="/Pizzavita/Ressources/evenement2.jfif">
+            <img class="imagesPhototèque" alt="../Ressources/evenement1.jfif" src="/Pizzavita/Ressources/evenement1.jfif">
+            <img class="imagesPhototèque" alt="../Ressources/evenement2.jfif" src="/Pizzavita/Ressources/evenement2.jfif">
         </div>
     </body>
 </html>
